d921a61ab96c2782e08840ef59ff7157f7c08bca
tangellaz <tangellaz@gmail.com>
2023-02-03 11:33:17 -0700

    custom dropdown for user select

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import styles from "../styles/Home.module.css";
 import Chef from "../components/chef/chef";
@@ -7,6 +7,8 @@ import Owner from "../components/owner/owner";
 import Footer from "../components/footer/footer";
 
 import { useGetDataQuery } from "../lib/api";
+
+import { titleCase } from "../lib/utils";
 
 import { useAppSelector, useAppDispatch } from "../redux/hooks";
 import { setAssembledPizzas } from "../redux/assembled-pizzas.slice";
@@ -19,9 +21,12 @@ export default function Home() {
   const { data, error, isLoading, isFetching, isSuccess } = useGetDataQuery();
   // console.log("data", data);
   const dispatch = useAppDispatch();
-
+  const userOptionsRef = useRef<HTMLDivElement>(null);
+  const dropdownsRef = useRef<HTMLUListElement>(null);
   const [user, setUser] = useState<string>("Select user");
+  const [showUserOptions, setShowUserOptions] = useState<boolean>(false);
   console.log("user", user);
+
   useEffect(() => {
     if (data) {
       dispatch(
@@ -38,6 +43,20 @@ export default function Home() {
     }
   }, [data]);
 
+  useEffect(() => {
+    const closeUserOptions = (e: MouseEvent) => {
+      if (
+        // e.target !== userOptionsRef.current ||
+        userOptionsRef.current &&
+        !userOptionsRef.current.contains(e.target as Node)
+      ) {
+        setShowUserOptions(false);
+      }
+    };
+    window.addEventListener("mousedown", closeUserOptions);
+    return () => window.removeEventListener("mousedown", closeUserOptions);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -49,9 +68,50 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>It&apos;s Pizza Time</h1>
 
-        <div className={styles.userSelectContainer}>
+        <div className={styles.userSelectContainer} ref={userOptionsRef}>
           <label htmlFor="user-select">User:</label>
-          <select
+          <div className={styles.dropdownContainer}>
+            <div
+              className={styles.inputWrapper}
+              onClick={() => {
+                setShowUserOptions(!showUserOptions);
+                dropdownsRef.current && dropdownsRef.current.focus();
+              }}
+            >
+              <input id="user-select" readOnly value={titleCase(user)} />
+              <img
+                src="/expand.svg"
+                decoding="async"
+                width="12"
+                height="7.1"
+                alt="expand"
+              />
+            </div>
+            {showUserOptions ? (
+              <ul
+                className={styles.dropdowns}
+                ref={dropdownsRef}
+                // onBlur={() => setShowUserOptions(false)}
+                // onMouseOut={() => setShowUserOptions(false)}
+                tabIndex={0}
+              >
+                {["Owner", "Chef", "Superuser"].map((userOption) =>
+                  userOption.toLowerCase() != user.toLowerCase() ? (
+                    <li
+                      key={userOption}
+                      onClick={() => {
+                        setUser(userOption.toLowerCase());
+                        setShowUserOptions(false);
+                      }}
+                    >
+                      {userOption}
+                    </li>
+                  ) : null
+                )}
+              </ul>
+            ) : null}
+          </div>
+          {/*          <select
             id="user-select"
             name="user"
             onChange={(e) => setUser(e.target.value.toLowerCase())}
@@ -64,7 +124,7 @@ export default function Home() {
                 {user}
               </option>
             ))}
-          </select>
+          </select>*/}
         </div>
 
         <div className={styles.userContainer}>
